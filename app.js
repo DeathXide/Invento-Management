e09bf161892dad684bd25cc9d2b6f66499ba7333
Manasi <manasi.s224@gmail.com>
2021-09-24 11:24:11 +0530

    Add gitignore

--- app.js
+++ app.js
@@ -375,6 +375,13 @@ app.post("/deleteP",function(req,res){
 
 
 // Server
-app.listen(3000,function(){
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function(){
     console.log("Server is running")
 })
